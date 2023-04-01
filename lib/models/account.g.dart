6a7fd155f4e2f11f31561a60ac4165063906ea61
Aditya <aditya.tawade@somaiya.edu>
2023-04-01 16:43:23 +0530

    created accounts model

---
+++ lib/models/account.g.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'account.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class AccountAdapter extends TypeAdapter<Account> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Account read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Account(
+      fields[0] as String,
+      fields[1] as String,
+      fields[2] as String,
+      fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Account obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.accountAddress)
+      ..writeByte(1)
+      ..write(obj.privateKey)
+      ..writeByte(2)
+      ..write(obj.name)
+      ..writeByte(3)
+      ..write(obj.category);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is AccountAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
