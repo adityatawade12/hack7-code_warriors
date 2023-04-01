78e66bdfbe582977d2526b71cd3fa47971f41cda
Aditya <aditya.tawade@somaiya.edu>
2023-04-01 15:57:20 +0530

    animation init added

--- lib/screens/account/CreateNewAccount.dart
+++ lib/screens/account/CreateNewAccount.dart
@@ -17,6 +17,50 @@ class _CreateNewAccountState extends State<CreateNewAccount>
   double topBarOpacity = 0.0;
 
   @override
+  void initState() {
+    animationController = AnimationController(
+        duration: const Duration(milliseconds: 1000), vsync: this);
+
+    topBarAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: animationController!,
+            curve: const Interval(0, 0.5, curve: Curves.fastOutSlowIn)));
+
+    addAllListData();
+
+    scrollController.addListener(() {
+      if (scrollController.offset >= 24) {
+        if (topBarOpacity != 1.0) {
+          setState(() {
+            topBarOpacity = 1.0;
+          });
+        }
+      } else if (scrollController.offset <= 24 &&
+          scrollController.offset >= 0) {
+        if (topBarOpacity != scrollController.offset / 24) {
+          setState(() {
+            topBarOpacity = scrollController.offset / 24;
+          });
+        }
+      } else if (scrollController.offset <= 0) {
+        if (topBarOpacity != 0.0) {
+          setState(() {
+            topBarOpacity = 0.0;
+          });
+        }
+      }
+    });
+    super.initState();
+  }
+
+  void addAllListData() {
+    const int count = 10;
+    // listViews.add(SizedBox(
+    //   height: 100,
+    // ));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container();
   }
